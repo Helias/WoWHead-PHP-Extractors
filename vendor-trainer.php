93c5a0a3eec54430ce05ccaecb89c48c798374ba
Helias <stefano520@hotmail.it>
2014-09-15 13:48:34 +0200

    Updated vendor-trainer.php now it works ;)

--- vendor-trainer.php
+++ vendor-trainer.php
@@ -1,34 +1,32 @@
 <?php
 include 'connect.php';
 
-//$querys=mysql_query("SELECT entry FROM creature_template WHERE entry IN (SELECT id FROM creature WHERE id IN (SELECT entry FROM creature_template WHERE entry>0 AND npcflag=0)) ORDER BY entry;");
-$querys=mysql_query("SELECT entry FROM creature_template WHERE entry IN (SELECT id FROM creature WHERE id IN (SELECT entry FROM creature_template WHERE entry>0 AND npcflag=0)) ORDER BY entry;");
+//$querys=mysql_query("SELECT entry FROM creature_template WHERE entry IN (SELECT id FROM creature WHERE id IN (SELECT entry FROM creature_template WHERE entry > $start)) ORDER BY entry;");
+$querys=mysql_query("SELECT entry, npcflag FROM creature_template WHERE entry = $start ORDER BY entry;");
 
 while ($executes=mysql_fetch_array($querys, MYSQL_ASSOC))
 {
 	echo "\n Checking {$executes['entry']}";
-	$query=mysql_query("SELECT npcflag FROM creature_template WHERE entry={$executes['entry']};");
-	$data=file_get_contents("http://www.wowhead.com/npc={$executes['entry']};");
-	if (strpos($data,"'sells'"))
+	$data=getHTML("http://www.wowhead.com/npc={$executes['entry']};", 10);
+	if (strpos($data,"tab_sells"))
 	{
-		while ($execute = mysql_fetch_array($query, MYSQL_ASSOC)){$npcflag=$execute['npcflag'];}
-		if ($npcflag=="0")
+		if ($executes['npcflag'] == "0")
 		{
-			$file=fopen("Vendor.sql", "a+");
+			$file=fopen("Vendor.txt", "a+");
 			fwrite($file, "\n{$executes['entry']}");
 			fclose($file);
-			echo "\n Found bugged: {$executes['entry']}!";
+			echo "  - vendor"; // if the NPC is bugged print this
 		}
 	}
-	elseif (strpos($data,"''teaches-recipe'"))
+	
+	if (strpos($data,"tab_teaches"))
 	{
-		while ($execute = mysql_fetch_array($query, MYSQL_ASSOC)){$npcflag=$execute['npcflag'];}
-		if ($npcflag=="0")
+		if ($executes['npcflag'] == "0")
 		{
-			$file=fopen("TrainerCode.sql", "a+");
+			$file=fopen("Trainer.txt", "a+");
 			fwrite($file, "\n{$executes['entry']}");
 			fclose($file);
-			echo "\n Found bugged: {$executes['entry']}!";
+			echo " -- trainer"; // if the NPC is bugged print this
 		}
 	}
 }
